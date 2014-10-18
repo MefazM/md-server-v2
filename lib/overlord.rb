10f711e71edc20bedff9eec5f07dced6bf8c660d
Egench <egnech@gmail.com>
2014-10-18 17:14:37 +0300

    refactor actions queue to bloking thread safe(?) queue

--- lib/overlord.rb
+++ lib/overlord.rb
@@ -18,7 +18,7 @@ class Overlord
     end
 
     def push_action(data)
-      @requests_queue << data
+      @queue << data
     end
 
     def [](name)
@@ -29,37 +29,30 @@ class Overlord
     end
 
     def run!
-      @requests_queue = ThreadSafe::Array.new
+      @queue = Queue.new
       @actors = ThreadSafe::Cache.new
 
       @threads = []
 
       @num_threads.to_i.times do
-        @threads << Thread.new {
-          loop {
-            request = @requests_queue.pop
+        @threads << Thread.new do
+          loop do
+            # begin
+            name, action, payload = @queue.deq
+            @actors[name].method(action).call(payload)
 
-            if request
-              # begin
-                name, action, payload = request
+            # rescue Exception => e
+            #   TheLogger.error <<-MSG
+            #     Can't call actor by name= '#{name}', action: '#{action}'
+            #     #{e}
+            #     #{e.backtrace.join('\n')}
+            #   MSG
+            # end
+          end
+        end
 
-                @actors[name].method(action).call(payload)
-
-              # rescue Exception => e
-              #   TheLogger.error <<-MSG
-              #     Can't call actor by name= '#{name}', action: '#{action}'
-              #     #{e}
-              #     #{e.backtrace.join('\n')}
-              #   MSG
-              # end
-
-            else
-              #TODO: use blocking queue instead
-              sleep 0.0001
-            end
-          }
-        }
       end
     end
+
   end
 end
